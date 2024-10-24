8a2278133fca8306e8f2583b5509556696347b11
gdsaudb <sven.sjoqvist14@gmail.com>
2024-10-24 19:02:08 +0200

    finally

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/api(.*)",
   "/book(.*)",
+  "/contact(.*)",
+  "/surfcamp(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {

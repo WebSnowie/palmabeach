1c57edd7c01d6247c13366fde059c755367e6903
WebSnowie <165617978+WebSnowie@users.noreply.github.com>
2024-10-27 16:04:09 +0100

    Nextjs Speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans bg-background`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
     </ClerkProvider>
